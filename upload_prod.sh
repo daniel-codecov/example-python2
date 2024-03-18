3c4482f0a428b0ec7e652332a9f927d39079d092
Daniel Yu <daniel.yu@sentry.io>
2024-03-18 17:43:10 -0400

    commit1

--- upload_prod.sh
+++ upload_prod.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-codecovcli create-commit -t 663eb710-98ff-4051-b11b-9a43710ca5cf 
-codecovcli create-report -t 663eb710-98ff-4051-b11b-9a43710ca5cf 
-codecovcli do-upload --file coverage/coverage_x.xml -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F x --disable-search
-codecovcli do-upload --file coverage/coverage_y.xml -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F y --disable-search
-codecovcli do-upload --file coverage/coverage_z.xml -t 663eb710-98ff-4051-b11b-9a43710ca5cf -F z --disable-search
+codecovcli create-commit -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad
+codecovcli create-report -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad
+codecovcli do-upload --file coverage/coverage_x.xml -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad -F x --disable-search
+codecovcli do-upload --file coverage/coverage_y.xml -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad -F y --disable-search
+codecovcli do-upload --file coverage/coverage_z.xml -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad -F z --disable-search
 
