98c9ebb19df5daa573ee49957170eba0c83bffd6
Daniel Yu <daniel.yu@sentry.io>
2024-03-18 17:44:48 -0400

    commit2

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -45,12 +45,12 @@ def test_sqrt():
     assert Calculator.sqrt(2, 0.0000001) == pytest.approx(1.41421356237, 1e-7)
 
 
-# def test_pow():
-#     assert Calculator.pow(2, 2) == 4
-#     assert Calculator.pow(1, 2) == 1
-#     assert Calculator.pow(4, 2) == 16
-#     assert Calculator.pow(3, 3) == 27
-#     assert Calculator.pow(10, 5) == 100000
+def test_pow():
+    assert Calculator.pow(2, 2) == 4
+    assert Calculator.pow(1, 2) == 1
+    assert Calculator.pow(4, 2) == 16
+    assert Calculator.pow(3, 3) == 27
+    assert Calculator.pow(10, 5) == 100000
 
 
 def test_minus():
@@ -59,5 +59,5 @@ def test_minus():
     assert Calculator.minus(0.5) == -0.5
     assert Calculator.minus(0) == 0
 
-# def test_random_function():
-#     assert Calculator.random_function() == 0
+def test_random_function():
+    assert Calculator.random_function() == 0
--- upload_prod.sh
+++ upload_prod.sh
@@ -3,6 +3,6 @@
 codecovcli create-commit -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad
 codecovcli create-report -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad
 codecovcli do-upload --file coverage/coverage_x.xml -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad -F x --disable-search
-codecovcli do-upload --file coverage/coverage_y.xml -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad -F y --disable-search
-codecovcli do-upload --file coverage/coverage_z.xml -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad -F z --disable-search
+# codecovcli do-upload --file coverage/coverage_y.xml -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad -F y --disable-search
+# codecovcli do-upload --file coverage/coverage_z.xml -t 2f7dd978-f9b4-4c13-b783-b4cfc79b1aad -F z --disable-search
 
